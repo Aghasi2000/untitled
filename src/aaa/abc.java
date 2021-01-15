0c6467b6a4f67a21f6c037fc6385b04dd78d76e8
Aghasi2000 <aghasi.mkrtchyan2@gmail.com>
2021-01-15 15:27:06 +0400

    Init

--- src/aaa/abc.java
+++ src/aaa/abc.java
@@ -1,7 +1,21 @@
 package aaa;
 
+import java.util.*;
+
 public class abc {
     public static void main(String[] args) {
-        System.out.println("aaa");
+     y o1 = new y();
+        System.out.println(o1.a1);
     }
+
 }
+
+
+    class x{
+    int a1 = 10;
+    String s1;
+    }
+
+    class y extends x{
+    int a1 = 11;
+    }
